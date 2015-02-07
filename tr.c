620ddfe3ebfaf56634ac4473535a2c30262bdd64
Tyler Massicotte <massicottetyler@gmail.com>
2015-02-06 21:49:21 -0800

    added comments
    Commented most of the functions

--- tr.c
+++ tr.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXSIZE 512
 
 
-
+/*Author: Tyler Massicotte A00855150 Set D
+* Email: MassicotteTyler@gmail.com
+* This program is a simplified verision of the unix command, 
+* which is translate. The program translates characters from the 
+* first set the equilivant character in the second set. 
+* Ex a1.exe abc xyz
+*/
 void handle_escape(const char o[], char n[]); /*old, new */
-void read_char(char s[]);
 void handle_range(char s[]);
 void del(const char s[]);
 void translate(const char set1[], const char set2[]);
@@ -16,13 +22,19 @@ int  detect_range(const char set[]);
 int  check_argc(int arg);
 int  handle_flag(char *arg);
 
+/*
+* Main Function: Starting point of the program, takes in command
+* line arguments. Number of arguments is represented as argc.
+* The commands themselves are represented in char pointers as argv.
+* The multiple if statements process what conditions the user as 
+* passed in through the command line and handles them accordingly.
+*/
 int main(int argc, char *argv[]) {
 
-	char input1[MAXSIZE];
+	char input1[MAXSIZE]; /*Char array to store the string from argv*/
 	char input2[MAXSIZE];
 	char *set1;
 	char *set2;
-	char *set3;
 
 	if(check_argc(argc) == 3 && handle_flag(argv[1]) == -1) {
 		set1 = argv[1];
@@ -73,7 +85,9 @@ int check_argc(int arg) {
 	else
 		return 2;
 }
-
+/*Takes in an argv and detects if it is -t or -d.
+* Returns 1 if -t, 0 if -d and -1 if neither.
+*/
 int handle_flag(char *arg) {
 	char str1[] = "-t";
 	char str2[] = "-d";
@@ -85,15 +99,11 @@ int handle_flag(char *arg) {
 	return -1;
 }
 
-void read_char(char s[]) {
-	int i;
-	int a = sizeof(s);
-	for (i = 0; i < a; i++) {
-		/*if (s[i] == '\')*/
-	}
-
-}
-
+/*Takes in 2 sets of strings. Gets characters via stdin and compares
+* the character to the characters in set1, if there is a match it 
+* replaces the character with the character in the equilivant position
+* in set2
+*/
 void translate(const char set1[], const char set2[]) {
 	int c;
 	int i;
@@ -115,6 +125,11 @@ void translate(const char set1[], const char set2[]) {
 
 }
 
+/*
+* Takes in a char array s[]. Gets characters from stdin and compares
+* them with the chars in s. If there is a match the char is not printed
+* and the next iteration of the loop is started.
+*/
 void del(const char s[]) {
 	int c;
 	int i;
@@ -132,6 +147,12 @@ void del(const char s[]) {
 	}
 }
 
+/*Takes in two char arrays, o(ld) and n(ew). The array o is looped through
+* and looks for a '\' character. If found it checks the next character and
+* compares them to acceptable cases in the switch. If an acceptable case
+* is found it replaces the '\' character with the appropiate escape char
+* e.g. 'a\t' becomes 'a    '
+*/
 void handle_escape(const char o[], char n[]) {
 	size_t i;
 	size_t x = 0;
@@ -167,7 +188,7 @@ void handle_escape(const char o[], char n[]) {
 					n[x] = '\v';
 					break;
 				case '\'': 
-					n[x] = "\'";
+					n[x] = '\'';
 					break;
 				case '\"': 
 					n[x] = '\"';
@@ -181,6 +202,11 @@ void handle_escape(const char o[], char n[]) {
 
 }
 
+/*Detects if there is a - character. If a - character is detected it
+* gets the value of the value of the character before it and after. It
+* then writes to a new array all the values from the first character to
+* the last inclusively. Then the new array is copied to the old one.
+*/
 void handle_range(char n[]) {
 	char temp[MAXSIZE];
 	size_t i;
@@ -188,6 +214,10 @@ void handle_range(char n[]) {
 	int z = 0;
 	for (i = 0; i < strlen(n); i++) {
 		if (n[i] == '-' && i != 0 && n[i+1] != '-') {
+			if (n[i-1] > n[i+1]) {
+				fprintf(stderr, "Error, first char of range exceeds second");
+				exit(1);
+			}
 			for (x = n[i-1]; x <= n[i+1]; x++, z++) {
 				temp[z] = x;
 			}
@@ -198,7 +228,7 @@ void handle_range(char n[]) {
 }
 
 void expand(const char set1[], char set2[]) {
-	size_t i, x;
+	size_t i;
 	char temp;
 	if (strlen(set1) > strlen(set2)) {
 		temp = set2[strlen(set2) - 1];
